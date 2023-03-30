9ee54321fa86aed66042210e31eee7162f6bdce1
Umer <umerqaisar786@gmail.com>
2023-03-30 12:41:52 +0500

    Added validations for group and liaison model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,10 @@
 class Group < ApplicationRecord
+  belongs_to :leader, optional: true
+
+  validates :name, :group_type, :size, :description, presence: true
+  validates_numericality_of :size, less_than_or_equal_to: 10
+
+  def leader_name
+    leader ? leader.full_name : 'No Leader for this Group'
+  end
 end
