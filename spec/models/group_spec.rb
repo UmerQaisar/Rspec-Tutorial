1d3fa7eaf84d370c3be131f210a9da28ea0392c4
Umer <umerqaisar786@gmail.com>
2023-03-30 13:57:28 +0500

    Wrote specs for Models: Group and Leader

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:group) { FactoryBot.build(:group) }
+  let(:leader) { FactoryBot.build(:leader) }
+
+  context 'Should validate' do
+    it 'with name, description, group type and size present' do
+      expect(group).to be_valid
+    end
+
+    it 'when leader is not present' do
+      group.leader = nil
+      expect(group).to be_valid
+    end
+  end
+
+  context 'Should not be valid' do
+    it 'when name is not present' do
+      group.name = nil
+      expect(group).not_to be_valid
+    end
+
+    it 'when description is not present' do
+      group.description = nil
+      expect(group).not_to be_valid
+    end
+
+    it 'when group type is not present' do
+      group.group_type = nil
+      expect(group).not_to be_valid
+    end
+  end
+
+  context 'Size' do
+    it 'should not be valid when size is not present' do
+      group.size = nil
+      expect(group).not_to be_valid
+    end
+
+    it 'should not be valid when size is greater than 10' do
+      group.size = 12
+      expect(group).not_to be_valid
+    end
+
+    it 'should valid when size is equal to 10' do
+      group.size = 10
+      expect(group).to be_valid
+    end
+
+    it 'should valid when size is lesser than 10' do
+      group.size = 8
+      expect(group).to be_valid
+    end
+  end
+
+  context 'Leader Name' do
+    it 'should return No Liaison for this Group when leader is not present' do
+      group.leader = nil
+      expect(group.leader_name).to eq 'No Leader for this Group'
+    end
+
+    it 'should return leader full name when leader is present' do
+      leader.first_name = 'Umer'
+      leader.last_name = 'Qaisar'
+      group.leader = leader
+      expect(group.leader_name).to eq 'Umer Qaisar'
+    end
+  end
 end
--- spec/models/leader_spec.rb
+++ spec/models/leader_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Leader, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:leader) { FactoryBot.build(:leader) }
+  let(:group) { FactoryBot.build(:group) }
+
+  context 'Should validate' do
+    it 'with first name, last name, email and phone number present' do
+      expect(leader).to be_valid
+    end
+
+    # it 'when group is not present' do
+    #   leader.group = nil
+    #   expect(group).to be_valid
+    # end
+  end
+
+  context 'Should not be valid' do
+    it 'when first name is not present' do
+      leader.first_name = nil
+      expect(leader).not_to be_valid
+    end
+
+    it 'when last name is not present' do
+      leader.last_name = nil
+      expect(leader).not_to be_valid
+    end
+
+    it 'when email is not present' do
+      leader.email = nil
+      expect(leader).not_to be_valid
+    end
+
+    it 'when phone number is not present' do
+      leader.phone_number = nil
+      expect(leader).not_to be_valid
+    end
+  end
+
+  context 'Phone number format' do
+    it 'should not be valid when phone number is of wrong format' do
+      leader.phone_number = '3367777025'
+      expect(leader).not_to be_valid
+    end
+
+    it 'should be valid when phone number is of right format' do
+      leader.phone_number = '336-777-7025'
+      expect(leader).to be_valid
+    end
+  end
+
+  context 'Email format' do
+    it 'should not be valid when email is of wrong format' do
+      leader.email = 'umer_qaisar.com'
+      expect(leader).not_to be_valid
+    end
+
+    it 'should be valid when email is of right format' do
+      leader.email = 'umerqaisar786@gmail.com'
+      expect(leader).to be_valid
+    end
+  end
+
+  context 'Full Name' do
+    it 'should expect leader full name to be Umer Qaisar' do
+      leader.first_name = 'Umer'
+      leader.last_name = 'Qaisar'
+      expect(leader.full_name).to eq 'Umer Qaisar'
+    end
+  end
+
+  context 'Group Name' do
+    it 'should return No Group for this Leader when group is not present' do
+      leader.group = nil
+      expect(leader.group_name).to eq 'No Group for this Leader'
+    end
+
+    it 'should return Name of the group when group is present' do
+      group.name = 'GCU'
+      leader.group = group
+      expect(leader.group_name).to eq 'GCU'
+    end
+  end
 end
