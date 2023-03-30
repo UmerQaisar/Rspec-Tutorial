4c270991308ef0ce69998af9d24b491e324ae052
Umer <umerqaisar786@gmail.com>
2023-03-30 12:54:35 +0500

    Filled factories for groups and leaders

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :group do
-    
+    name { 'GCU' }
+    group_type { 'College' }
+    description { 'Group to connect people' }
+    size { 8 }
+    leader
   end
 end
